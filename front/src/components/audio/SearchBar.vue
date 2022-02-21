5f319db268385463368ae058b754fb4123bf4685
Kasper Seweryn <github@wvffle.net>
2022-02-21 23:48:55 +0100

    Remove console.log

--- front/src/components/audio/SearchBar.vue
+++ front/src/components/audio/SearchBar.vue
@@ -25,7 +25,6 @@ import jQuery from 'jquery'
 import router from '@/router'
 import lodash from 'lodash'
 import GlobalEvents from '@/components/utils/global-events.vue'
-console.log(jQuery)
 
 export default {
   components: {
