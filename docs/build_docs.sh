bedfcdcf191b1b9ea5ce0df41e7b6e550a749521
Georg Krause <mail@georg-krause.net>
2022-03-02 14:00:44 +0100

    Generate docs for stable and develop

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 # Building sphinx and swagger docs
-python -m sphinx . $BUILD_PATH
+poetry run sphinx-multiversion . $BUILD_PATH
 TARGET_PATH="$BUILD_PATH/swagger" ./build_swagger.sh
 python ./get-releases-json.py > $BUILD_PATH/releases.json
 python ./get-releases-json.py --latest > $BUILD_PATH/latest.txt
