7e7cd25fa32b4fb4c7a37be4e7c8188f151fe4fc
JuniorJPDJ <git@juniorjpdj.pl>
2022-07-16 20:18:06 +0200

    feat(api/Docker): automatically run migrations on container start
    migrations do nothing when there's nothing to do and this allows easier version upgrades

--- api/compose/django/server.sh
+++ api/compose/django/server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -eux
 python /app/manage.py collectstatic --noinput
+python /app/manage.py migrate
 gunicorn config.asgi:application -w ${FUNKWHALE_WEB_WORKERS-1} -k uvicorn.workers.UvicornWorker -b 0.0.0.0:5000 ${GUNICORN_ARGS-}
