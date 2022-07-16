94fcd48f2a3bb265588f76b5a597ac4b3411b797
JuniorJPDJ <git@juniorjpdj.pl>
2022-07-16 18:17:26 +0200

    fix(flake8): disable error on api/config/__init__.py import

--- api/config/__init__.py
+++ api/config/__init__.py
@@ -1,1 +1,3 @@
-import config.schema
+# loads what is required to generate the swagger docs
+# https://matrix.to/#/!nNBDNverFlbfNpReEO:matrix.org/$16579878472182UmZUv:tchncs.de?via=tchncs.de&via=matrix.org&via=juniorjpdj.pl
+import config.schema  # noqa: F401
