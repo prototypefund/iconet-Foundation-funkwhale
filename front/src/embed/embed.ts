359752736261d39b1b72623ed3c3fdc93f910afd
wvffle <funkwhale@wvffle.net>
2022-09-26 17:45:27 +0000

    Implement embedded player logic

--- front/src/embed/embed.ts
+++
@@ -1,9 +0,0 @@
-import EmbedFrame from './EmbedFrame.vue'
-import { createApp } from 'vue'
-
-// @ts-expect-error vue-plyr has no types defined
-import VuePlyr from 'vue-plyr'
-
-const app = createApp(EmbedFrame)
-app.use(VuePlyr)
-app.mount('#app')
