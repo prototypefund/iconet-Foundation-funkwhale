f60ba2d9be7187f910e8d0c6b4c12d43d65109c5
jo <ljonas@riseup.net>
2022-11-26 14:29:57 +0100

    fix: docker nginx entrypoint
    Part-of: <https://dev.funkwhale.audio/funkwhale/funkwhale/-/merge_requests/2217>

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -2,10 +2,10 @@
 
 set -eux
 
-envsubst "$(env | awk -F = '{printf \" $$%s\", $$1}')" \
-  < /etc/nginx/nginx.conf.template \
-  > /etc/nginx/nginx.conf
+TEMPLATE_PATH="/etc/nginx/nginx.conf.template"
+CONFIG_PATH="/etc/nginx/nginx.conf"
 
-cat /etc/nginx/nginx.conf
+ALLOWED_VARS="$(env | cut -d '=' -f 1 | xargs printf "\${%s} ")"
+envsubst "$ALLOWED_VARS" < "$TEMPLATE_PATH" | tee "$CONFIG_PATH"
 
 nginx-debug -g 'daemon off;'
