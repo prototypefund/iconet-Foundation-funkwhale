05cb102b2182d79c5770b5c87258d4bc148636b8
JuniorJPDJ <git@juniorjpdj.pl>
2022-12-12 18:55:39 +0000

    fix(ci): fix CI `build_documentation` job swagger-UI generation and remove leftovers NOCHANGELOG

--- docs/build_swagger.sh
+++ docs/build_swagger.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
-
 set -eux
 
-SWAGGER_VERSION="4.1.3"
+SWAGGER_VERSION="4.15.5"
 TARGET_PATH=${TARGET_PATH-"swagger"}
 
-rm -rf "$TARGET_PATH" /tmp/swagger-ui
-git clone --branch="v$SWAGGER_VERSION" --depth=1 "https://github.com/swagger-api/swagger-ui.git" /tmp/swagger-ui
+rm -rf "$TARGET_PATH"
+tmpdir="$(mktemp -d)"
+trap 'rm -rf "$tmpdir"' EXIT
 
-mv /tmp/swagger-ui/dist "$TARGET_PATH"
+pushd "$tmpdir"
+wget "https://github.com/swagger-api/swagger-ui/archive/refs/tags/v$SWAGGER_VERSION.tar.gz" -O swagger-ui.tgz
+tar -xzf swagger-ui.tgz
+popd
+mv "$tmpdir/"*/dist "$TARGET_PATH"
 cp schema.yml "$TARGET_PATH"
-cp -r api "$TARGET_PATH/api"
-sed -i "s,https://petstore.swagger.io/v2/swagger.json,schema.yml,g" "$TARGET_PATH/index.html"
+
+sed -i "s#https://petstore.swagger.io/v2/swagger.json#schema.yml#g" "$TARGET_PATH/swagger-initializer.js"
